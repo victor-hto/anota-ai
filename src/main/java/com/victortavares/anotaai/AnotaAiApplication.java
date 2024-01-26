c51a36c4fa89c6ca11ca284bfd4f1769d2290085
Victor Tavares <ti@assempbh.org.br>
2024-01-25 21:42:01 -0300

    Iniciando projeto

---
+++ src/main/java/com/victortavares/anotaai/AnotaAiApplication.java
@@ -0,0 +1,13 @@
+package com.victortavares.anotaai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotaAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnotaAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victortavares/anotaai/AnotaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victortavares.anotaai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotaAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
